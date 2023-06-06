6258a22fef5f299f367d8fd743aadf92d41f2324
Lucas Shangrita <oustanilokman@gmail.com>
2023-06-06 17:53:07 +0300

    Add page animation

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.addEventListener('load', function() {
     var logo = document.getElementsByClassName('logo-img');
     logo.style.animationPlayState = 'running';
   });
+
+const observer = new IntersectionObserver ((entries) => {
+entries.forEach((entry) => {
+  console.log(entry)
+  if(entry.isIntersecting){
+    entry.target.classList.add('show');
+  } else{
+    entry.target.classList.remove('show');
+  }
+});
+});
+
+const hiddenElements = document.querySelectorAll('.hiden');
+hiddenElements.forEach((el)=> observer.observe(el));
